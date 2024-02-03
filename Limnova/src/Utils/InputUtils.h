f6143c057b6b0fb771ced43225b87c068c9de3b0
Sven Buckland <svenbuckland@gmail.com>
2024-02-03 15:55:31 +0000

    Templatized existing utility functions which convert between unsigned integer types and ascii strings: requires explicit specialization; specializations included for uint64_t and uint32_t.

--- Limnova/src/Utils/InputUtils.h
+++ Limnova/src/Utils/InputUtils.h
@@ -10,33 +10,27 @@ namespace Limnova
 		template<typename NUMERIC>
 		constexpr size_t MaxAsciiCharacters()
 		{
-			LV_CORE_ERROR("MaxAsciiCharacters(): unrecognised type");
+			LV_CORE_ERROR("MaxAsciiCharacters(): unsupported type");
 			return 0;
 		}
 
-		/// <summary>
-		/// Convert an ASCII encoding of a decimal number to uint64_t.
-		/// Note: 'value' is unchanged if conversion fails.
-		/// </summary>
-		/// <param name="pData">Source of ASCII data</param>
-		/// <param name="length">Length of ASCII data (number of characters)</param>
-		/// <param name="value">Storage for output value</param>
-		/// <returns>True if successfully converted, otherwise false</returns>
-		ResultCode ConvertAsciiDecimalToUint64(const char *pData, size_t length, uint64_t &value);
+		template<typename NUMERIC>
+		ResultCode AsciiDecimalToUInt(const char* pData, size_t length, NUMERIC& value)
+		{
+			LV_CORE_ERROR("ConvertAsciiDecimalToNumeric(): unsupported type");
+			return 0;
+		}
 
-		/// <summary>
-		/// Encode a uint64_t as a decimal number into ASCII representation.
-		/// Note: 'pBuffer' and 'dataLength' are unchanged if conversion fails.
-		/// </summary>
-		/// <param name="value">Value to encode</param>
-		/// <param name="pBuffer">Destination of ASCII data</param>
-		/// <param name="bufferLength">Length of destination buffer (max number of characters)</param>
-		/// <param name="dataLength">Length of encoded representation (number of characters)</param>
-		/// <returns></returns>
-		ResultCode ConvertUint64ToAsciiDecimal(uint64_t value, char *pBuffer, size_t bufferLength, size_t &dataLength);
+		template<typename NUMERIC>
+		ResultCode UIntToAsciiDecimal(NUMERIC value, char* pBuffer, size_t bufferLength, size_t& dataLength)
+		{
+			LV_CORE_ERROR("ConvertNumericToAsciiDecimal(): unsupported type");
+			return 0;
+		}
 
 	}
 
+	// -------------------------------------------------------------------------------------------------------------------------
 	// -------------------------------------------------------------------------------------------------------------------------
 
 	template<>
@@ -44,4 +38,21 @@ namespace Limnova
 	{
 		return 20; // TODO : return log10(numeric_limits<uint64_t>::max()) (requires constexpr log10)
 	}
+
+	// -------------------------------------------------------------------------------------------------------------------------
+
+	template<>
+	constexpr size_t Utils::MaxAsciiCharacters<uint32_t>()
+	{
+		return 10; // TODO : return log10(numeric_limits<uint64_t>::max()) (requires constexpr log10)
+	}
+
+	// -------------------------------------------------------------------------------------------------------------------------
+
+	template<> ResultCode Utils::AsciiDecimalToUInt<uint64_t>(const char* pData, size_t length, uint64_t& value);
+	template<> ResultCode Utils::AsciiDecimalToUInt<uint32_t>(const char* pData, size_t length, uint32_t& value);
+
+	template<> ResultCode Utils::UIntToAsciiDecimal<uint64_t>(uint64_t value, char* pBuffer, size_t bufferLength, size_t& dataLength);
+	template<> ResultCode Utils::UIntToAsciiDecimal<uint32_t>(uint32_t value, char* pBuffer, size_t bufferLength, size_t& dataLength);
+
 }
