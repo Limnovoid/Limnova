ff2341811e7fff27ba2f170e3496c2ca08a9bec3
Sven Buckland <svenbuckland@gmail.com>
2024-02-05 20:14:32 +0000

    Added new physics query, SolveMissileInterceptVector(), which replicates the tracking logic from the Missile script - it is an example of generally useful navigation logic which should be available out-of-the-box in the query library.

--- LimnovaScriptCore/Source/Missile.cs
+++ LimnovaScriptCore/Source/Missile.cs
@@ -7,10 +7,9 @@ namespace Limnova
     public class Missile : Entity
     {
         public bool             Seek;
-        public uint             SeekMax;
+        public uint             SeekMaxIter;
         public float            SeekRecompFactor;
         public float            PropConstant;
-        public float            PropNavBias;
         public float            TargetingTolerance;
         public double           EngineThrust;
         public EntityReference  Target;
@@ -35,33 +34,20 @@ namespace Limnova
 
                 if (SeekTimer > 0.0f)
                 {
-                    // Accelerate toward intercept with a proportional navigation bias
+                    Native.OrbitalPhysics_SolveMissileInterceptVector(this.m_Id, Target.m_EntityId, EngineThrust, TargetingTolerance,
+                        SeekMaxIter, PropConstant, out Vec3 interceptVector, out Vec3 intercept, out float timeToIntercept);
 
-                    Native.OrbitalPhysics_SolveMissileIntercept(this.m_Id, Target.m_EntityId, EngineThrust, TargetingTolerance, SeekMax,
-                        out Vec3 intercept, out float timeToIntercept);
-
-                    SeekTimer -= timeToIntercept * SeekRecompFactor;
-
-                    Native.OrbitalPhysics_ComputeProportionalNavigationAcceleration(this.m_Id, Target.m_EntityId, PropConstant,
-                        out Vec3d proportionalAcceleration);
-
-                    Native.OrbitalPhysics_ComputeLocalAcceleration(this.m_Id, EngineThrust, out double localAcceleration);
-                    PropNavBias = Limnova.Math<float>.Clamp((float)(Math.Sqrt(proportionalAcceleration.SqrMagnitude()) / localAcceleration), 0.0f, 1.0f);
-
-                    Vec3 thrustDirection = ((1.0f - PropNavBias) * intercept.Normalized()) +
-                        (PropNavBias * (Vec3)(proportionalAcceleration.Normalized()));
-
-                    Vec3d thrustVector = new Vec3d(thrustDirection) * EngineThrust;
+                    Vec3d thrustVector = new Vec3d(interceptVector) * EngineThrust;
                     Native.OrbitalPhysics_SetThrust(this.m_Id, ref thrustVector);
 
                     // Place reticle
                     if (TargetingReticle.IsReferenceValid())
                     {
-                        Vec3 targetingReticlePosition = Transform.Position + intercept;
-
                         TransformComponent targetingReticleTransform = TargetingReticle.GetComponent<TransformComponent>();
-                        targetingReticleTransform.Position = targetingReticlePosition;
+                        targetingReticleTransform.Position = intercept;
                     }
+
+                    SeekTimer -= timeToIntercept * SeekRecompFactor;
                 }
             }
         }
